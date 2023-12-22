d81dc02095cf796492b5fecd573b4a190dd81adc
HwangGoeun <hwang_gim@naver.com>
2023-12-22 23:45:24 +0900

    foreground 구현

--- redtimer/lib/screens/home_screen.dart
+++ redtimer/lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:isolate';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void _initForegroundTask() {
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'foreground_service',
+        channelName: 'Foreground Service Notification',
+        channelDescription:
+            'This notification appears when the foreground service is running.',
+        channelImportance: NotificationChannelImportance.LOW,
+        priority: NotificationPriority.LOW,
+        iconData: const NotificationIconData(
+          resType: ResourceType.mipmap,
+          resPrefix: ResourcePrefix.ic,
+          name: 'launcher',
+        ),
+        buttons: [
+          const NotificationButton(id: 'sendButton', text: 'Send'),
+          const NotificationButton(id: 'testButton', text: 'Test'),
+        ],
+      ),
+      iosNotificationOptions: const IOSNotificationOptions(
+        showNotification: true,
+        playSound: false,
+      ),
+      foregroundTaskOptions: const ForegroundTaskOptions(
+        interval: 5000,
+        isOnceEvent: false,
+        autoRunOnBoot: true,
+        allowWakeLock: true,
+        allowWifiLock: true,
+      ),
+    );
+
+    noticeStart(format(totalSeconds));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initForegroundTask();
+  }
+
   static const tenMinutes = 600;
   int totalSeconds = tenMinutes;
   bool isRunning = false;
@@ -26,10 +69,13 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         totalSeconds = totalSeconds - 1;
       });
+      FlutterForegroundTask.updateService(
+          notificationText: format(totalSeconds));
     }
   }
 
   void onStartPressed() {
+    noticeStart(format(totalSeconds));
     timer = Timer.periodic(
       const Duration(seconds: 1),
       onTick,
@@ -37,6 +83,15 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       isRunning = true;
     });
+  }
+
+  void noticeStart(String initialText) async {
+    await FlutterForegroundTask.startService(
+      notificationTitle: 'Red Timer',
+      notificationText: initialText,
+      callback: startCallback,
+    );
+    setState(() {});
   }
 
   void onPausePreesed() {
@@ -47,11 +102,17 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void onReset() {
+    noticeStop();
     setState(() {
       isRunning = false;
       totalSeconds = tenMinutes;
     });
     timer.cancel();
+  }
+
+  void noticeStop() async {
+    await FlutterForegroundTask.clearAllData();
+    await FlutterForegroundTask.stopService();
   }
 
   String format(int seconds) {
@@ -62,67 +123,99 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 3,
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              alignment: Alignment.bottomCenter,
-              decoration: const BoxDecoration(),
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).textTheme.displayLarge!.color,
-                  fontSize: 68,
-                  fontWeight: FontWeight.w600,
+    return WithForegroundTask(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Column(
+          children: [
+            Flexible(
+              flex: 3,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                alignment: Alignment.bottomCenter,
+                decoration: const BoxDecoration(),
+                child: Text(
+                  format(totalSeconds),
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.displayLarge!.color,
+                    fontSize: 68,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Flexible(
-            flex: 3,
-            child: Container(
-              decoration: const BoxDecoration(),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: isRunning ? onPausePreesed : onStartPressed,
-                    icon: Icon(
-                      isRunning ? Icons.pause : Icons.play_arrow,
+            const SizedBox(
+              height: 15,
+            ),
+            Flexible(
+              flex: 3,
+              child: Container(
+                decoration: const BoxDecoration(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: isRunning ? onPausePreesed : onStartPressed,
+                      icon: Icon(
+                        isRunning ? Icons.pause : Icons.play_arrow,
+                      ),
+                      iconSize: 60,
+                      color: Theme.of(context).textTheme.displayLarge!.color,
                     ),
-                    iconSize: 60,
-                    color: Theme.of(context).textTheme.displayLarge!.color,
-                  ),
-                  IconButton(
-                    onPressed: onReset,
-                    icon: const Icon(
-                      Icons.square,
+                    IconButton(
+                      onPressed: onReset,
+                      icon: const Icon(
+                        Icons.square,
+                      ),
+                      iconSize: 39,
+                      color: Theme.of(context).textTheme.displayLarge!.color,
                     ),
-                    iconSize: 39,
-                    color: Theme.of(context).textTheme.displayLarge!.color,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          // Flexible(
-          //   flex: 1,
-          //   child: Container(
-          //     decoration: const BoxDecoration(),
-          //     height: double.infinity,
-          //     child: const Icon(Icons.home),
-          //   ),
-          // ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+}
+
+@pragma('vm:entry-point')
+void startCallback() {
+  // The setTaskHandler function must be called to handle the task in the background.
+  FlutterForegroundTask.setTaskHandler(FirstTaskHandler());
+}
+
+class FirstTaskHandler extends TaskHandler {
+  SendPort? _sendPort;
+
+  @override
+  void onStart(DateTime timestamp, SendPort? sendPort) async {
+    _sendPort = sendPort;
+
+    final customData =
+        await FlutterForegroundTask.getData<String>(key: 'customData');
+    print('customData: $customData');
+  }
+
+  @override
+  void onRepeatEvent(DateTime timestamp, SendPort? sendPort) async {
+    sendPort?.send(timestamp);
+  }
+
+  @override
+  void onDestroy(DateTime timestamp, SendPort? sendPort) async {}
+
+  @override
+  void onNotificationButtonPressed(String id) {
+    print('onNotificationButtonPressed >> $id');
+  }
+
+  @override
+  void onNotificationPressed() {
+    // FlutterForegroundTask.launchApp("/resume-route");
+    _sendPort?.send('onNotificationPressed');
   }
 }
